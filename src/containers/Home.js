503c53db946b928da495ba6466174c9af6ae5904
Thomas Deconinck <thomas@laclic.fr>
2017-03-19 22:34:00 +0100

    Add native-base package

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { Container, Header, Title, Content, Button, Left, Right, Body, Icon } from 'native-base'
 import { getTimestamp } from '../actions/demo'
 
 type Props = {
@@ -14,13 +15,29 @@ class Home extends React.Component {
 
   render () {
     return (
-      <View>
-        <Text>Home view</Text>
-        <Text>Demo redux : { this.props.demoText }</Text>
-        <TouchableOpacity onPress={this._onPress}>
-          <Text>Change demo text !</Text>
-        </TouchableOpacity>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+
+        <Content>
+          <View>
+            <Text>Home view</Text>
+            <Text>Demo redux : {this.props.demoText}</Text>
+            <TouchableOpacity onPress={this._onPress}>
+              <Text>Change demo text !</Text>
+            </TouchableOpacity>
+          </View>
+        </Content>
+      </Container>
     )
   }
 
