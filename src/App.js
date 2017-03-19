768d0fc2688161be2b49b433874c80f131f03fb6
Thomas Deconinck <thomas@laclic.fr>
2017-03-19 17:32:07 +0100

    Add redux :sparkles:

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import {
   NavigationProvider,
   StackNavigation
 } from '@expo/ex-navigation'
-
+import { Provider } from 'react-redux'
+import store from './store'
 import Router from './utils/router'
 
 export default class App extends React.Component {
   render () {
     return (
-      <NavigationProvider router={Router}>
-        <StackNavigation initialRoute={Router.getRoute('home')} />
-      </NavigationProvider>
+      <Provider store={store}>
+        <NavigationProvider router={Router}>
+          <StackNavigation initialRoute={Router.getRoute('home')} />
+        </NavigationProvider>
+      </Provider>
     )
   }
 }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,40 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { getTimestamp } from '../actions/demo'
+
+type Props = {
+  getTimestamp: Function,
+  demoText: ?number
+}
 
 class Home extends React.Component {
+  props: Props
+
   render () {
     return (
       <View>
         <Text>Home view</Text>
+        <Text>Demo redux : { this.props.demoText }</Text>
+        <TouchableOpacity onPress={this._onPress}>
+          <Text>Change demo text !</Text>
+        </TouchableOpacity>
       </View>
     )
   }
+
+  _onPress = (): void => {
+    this.props.getTimestamp()
+  }
 }
 
-export default Home
+const mapStateToProps = ({ demo }) => ({
+  demoText: demo.text
+})
+
+const mapDispatchToProps = dispatch => ({
+  getTimestamp: bindActionCreators(getTimestamp, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
